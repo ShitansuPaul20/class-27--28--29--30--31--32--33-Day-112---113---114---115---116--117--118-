3d71b249d39d77e7c4964c23e5d5a987251e31be
ShitansuPaul20 <hackerpauloji@gmail.com>
2026-03-31 00:56:31 +0530

    Deployment changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,15 +4,35 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const authRoutes = require('./routes/auth.routes');
 const songRoutes = require('./routes/song.routes');
+const path = require('path');
+
+const allowedOrigins = [
+    'http://localhost:5173', 
+    'https://your-app-name.onrender.com' 
+];
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: function (origin, callback) {
+       
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.indexOf(origin) === -1) {
+            return callback(new Error('CORS policy block: This origin is not allowed'), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/songs', songRoutes);
+
+// Isme :any ek variable name hai aur * wildcard hai
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
